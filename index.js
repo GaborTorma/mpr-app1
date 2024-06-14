56bdf90950511ca9b53f04533d39a74e4b3246a8
Gábor Torma <gabor@torma.dev>
2024-06-14 15:04:31 +0200

    use gt-packages

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-import p1 from '@gabortorma/mpr-package1'
+import p1 from '@gt-packages/mpr-package1'
+import p2 from '@gt-packages/mpr-package2'
 
-console.log('mpr-app1 loaded', p1)
+console.log('mpr-app1 loaded', p1, p2)
